63133bd6d3c11ffdff3d91a5560277b31d66b264
Raymond T. Hightower <rth@wisdomgroup.com>
2013-03-07 09:36:08 -0600

    beginning wish list document implementation

---
+++ Stage 3 - Start on Split View, with portrait button/iTunesWishListMaker/iTunesWishListMaker/CCFWishListsDocument.h
@@ -0,0 +1,16 @@
+//
+//  CCFWishListsDocument.h
+//  iTunesWishListMaker
+//
+//  Created by RTH on 3/7/13.
+//  Copyright (c) 2013 Subsequently & Furthermore, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFWishListsDocument : UIDocument
+@property (copy) NSMutableArray *mutableWishListDicts;
+-(void) addItemDict: (NSDictionary*) dict;
+-(void) removeItemDict: (NSDictionary*) dict;
+
+@end
---
+++ Stage 3 - Start on Split View, with portrait button/iTunesWishListMaker/iTunesWishListMaker/CCFWishListsDocument.m
@@ -0,0 +1,14 @@
+//
+//  CCFWishListsDocument.m
+//  iTunesWishListMaker
+//
+//  Created by RTH on 3/7/13.
+//  Copyright (c) 2013 Subsequently & Furthermore, Inc. All rights reserved.
+//
+
+#import "CCFWishListsDocument.h"
+
+@implementation CCFWishListsDocument
+
+
+@end
