7e02b44647e4b1fd9afe826338786792267e280d
Raymond T. Hightower <rth@wisdomgroup.com>
2013-03-07 13:26:50 -0600

    starting with stage four

---
+++ Stage 4 - Create and list local documents/iTunesWishListMaker/iTunesWishListMaker/CCFWishListViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCFWishListViewController.h
+//  iTunesWishListMaker
+//
+//  Created by RTH on 3/7/13.
+//  Copyright (c) 2013 Subsequently & Furthermore, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFWishListViewController : UIViewController
+
+@end
